4e57a814b164c5ee5bd8b931402a341fc3e5158b
Tyler Yahn <codingalias@gmail.com>
2015-04-13 21:07:48 -0700

    Added main init.pp manifest.

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+# == Class: authbind
+#
+# Manages the installation of Authbind.
+#
+# === Parameters
+#
+# [*version*]
+#   The version of the authbind package to install.  Valid values are a
+#   valid release (2.1.1 or 1.2.0) or latest. Defalut is latest.
+#
+# === Examples
+#
+# To install authbind_1.2.0:
+#
+#  class { 'authbind':
+#    version => '1.2.0',
+#  }
+#
+# === Authors
+#
+# Tyler Yahn <codingalias@moonshadowmobile.com>
+#
+# === Copyright
+#
+# Copyright 2015 Tyler Yahn
+#
+class authbind (
+  $version = latest,
+) {
+  $_version = hiera_hash("${module_name}::version", $version)
+
+  validate_re($_version, ['latest$', '^1.2.0$', '^2.1.1$',])
+
+  package { 'authbind':
+    ensure => $_version,
+  }
+}
